7448825b96a4b331fcae63a721183b9e43ede734
sth-kiwi <saltypotet@gmail.com>
2023-12-24 23:18:14 +0800

    yessi

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gcashAccount = 'Your Gcash Account';
-    
-    const gcashAccountElement = document.getElementById('gcashAccount');
-    gcashAccountElement.textContent = gcashAccount;
-  });
-  
