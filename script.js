f041e877f29a118b1afa1b746a8bd88e3ce25361
sth-kiwi <saltypotet@gmail.com>
2023-12-24 22:19:14 +0800

    es
    as

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // You can replace the placeholder with your actual gcash account
+    const gcashAccount = 'Your Gcash Account';
+    
+    // Display the gcash account on the page
+    const gcashAccountElement = document.getElementById('gcashAccount');
+    gcashAccountElement.textContent = gcashAccount;
+  });
+  
